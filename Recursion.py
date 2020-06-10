d745532d2b2ceb7170e9ba0e87dfe2c609a82e4b
Sahil Islam <thesahil.islam@gmail.com>
2020-06-10 10:17:28 +0530

    Added new square(x,y,w) function.
    We see that if we apply same recursive way to draw squares, we get back the Sierpinski triangle.

--- Recursion.py
+++ Recursion.py
@@ -18,7 +18,10 @@ clock = pygame.time.Clock()
 
 screen = pygame.display.set_mode((width, height))
 
+"""This specific method draws the Sierpinski triangle. (Both circle and square) """
 
+
+#
 def circle(x, y, r):
     pygame.draw.circle(screen, red, (int(x), int(y)), int(r), 1)
     if r > 2:
@@ -27,13 +30,24 @@ def circle(x, y, r):
         circle(x, y + 0.5 * r, r * 0.5)
 
 
+def square(x, y, w):
+    pygame.draw.rect(screen, red, [int(x - w / 2.), int(y - w / 2.), int(w), int(w)], 1)
+    if w > 2:
+        square(x + 0.5 * w, y, w * 0.5)
+        square(x - 0.5 * w, y, w * 0.5)
+        square(x, y - 0.5 * w, w * 0.5)
+
+
+#
+
 while True:
     screen.fill(white)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
-    circle(width / 2., height / 2., 300)
+
+    square(width / 2., height / 2., 300)
 
     pygame.display.update()
     clock.tick(100)
